696c521e627424ec8cd09af2b8110c2ef6ac6eef
codert0109 <thebluemoonsea@gmail.com>
2023-02-07 00:14:41 +0900

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,92 +1,118 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component, useEffect, useState } from "react";
 
-import { CometChat } from '@cometchat-pro/chat';
-import MDSpinner from "react-md-spinner";
-import config from './config';
-import ChatBox from './ChatBox';
-import ChatList from './ChatList';
+import config from "./config";
+import ChatBox from "./ChatBox";
+import ChatList from "./ChatList";
+import { Navbar } from "react-chat-elements";
 
 const agentUID = config.agentUID;
-const AGENT_MESSAGE_LISTENER_KEY = 'agent-listener'
+const AGENT_MESSAGE_LISTENER_KEY = "agent-listener";
 const limit = 30;
 
 const mockupdata = [
   {
-    receiver: 'John',
+    name: "John",
     messages: [
       {
-        type: 'receive', text: 'hello'
+        type: "receive",
+        text: "hello",
       },
       {
-        type: 'send', text: 'yeah, hello!'
-      }
-    ]
-  }
-]
+        type: "send",
+        text: "yeah, hello!",
+      },
+    ],
+  },
+];
 
 function App() {
   const [chatList, setChatList] = useState([]);
-  const [selectedChat, setSelectedChat] = useState({});
-  const [chatIsLoading, setChatIsLoading] = useState(false);
-  const [customerIsLoading, setCustomerIsLoading] = useState(false);
+  const [selectedChat, setSelectedChat] = useState([]);
 
   useEffect(() => {
     setChatList(mockupdata);
-    setSelectedChat(mockupdata[0]);
-  }, [])
+    setSelectedChat(mockupdata[0].messages);
+  }, []);
 
-  const onSelectChat = () => {
+  const onSelectChat = () => {};
 
-  }
-
-  const handleSubmit = (val) => {
-
-  }
-  console.log(mockupdata);
+  const handleSubmit = (val) => {};
 
   return (
-    <div className='container-fluid'>
-      <div className='row'>
-        <div className='col-md-2'></div>
+    <div className="container-fluid">
+      <Navbar
+        left={<div>Logo</div>}
+        center={<div>Home</div>}
+        right={<div>Contact</div>}
+        z
+        type="light"
+      />
+      <div className="row">
+        <div className="col-md-2"></div>
         <div className="col-md-8 h-100pr border rounded">
-          <div className='row'>
-            <div className='col-lg-8 col-xs-12 bg-light' style={{ height: 658 }}>
-              <div className='row p-3 bg-white'>
+          <div className="row">
+            <div
+              className="col-lg-8 col-xs-12 bg-light"
+              style={{ height: 658 }}
+            >
+              <div className="row p-3 bg-white">
                 <h2>Who you gonna chat with?</h2>
               </div>
-              <div className='row pt-5 bg-white' style={{ height: 530, overflow: 'auto' }}>
-                <ChatBox chat={selectedChat.messages} />
+              <div
+                className="row pt-5 bg-white"
+                style={{ height: 530, overflow: "auto" }}
+              >
+                <ChatBox chat={selectedChat} />
               </div>
-              <div className="row bg-light" style={{ bottom: 0, width: '100%' }}>
+              <div
+                className="row bg-light"
+                style={{ bottom: 0, width: "100%" }}
+              >
                 <form className="row m-0 p-0 w-100" onSubmit={handleSubmit}>
-
                   <div className="col-9 m-0 p-1">
-                    <input id="text"
+                    <input
+                      id="text"
                       className="mw-100 border rounded form-control"
                       type="text"
                       name="text"
-                      ref="message"
-                      placeholder="Type a message..." />
+                      placeholder="Type a message..."
+                    />
                   </div>
                   <div className="col-3 m-0 p-1">
-                    <button className="btn btn-outline-secondary rounded border w-100"
+                    <button
+                      className="btn btn-outline-secondary rounded border w-100"
                       title="Send"
-                      style={{ paddingRight: 16 }}>Send</button>
+                      style={{ paddingRight: 16 }}
+                    >
+                      Send
+                    </button>
                   </div>
                 </form>
               </div>
             </div>
-            <div className='col-lg-4 col-xs-12 bg-light' style={{ height: 658 }}>
-              <div className='row p-3'><h2>Recent Chat</h2></div>
-              <div className='row ml-0 mr-0 h-75 bg-white border rounded' style={{ height: '100%', overflow: 'auto' }}>
-                <ChatList chatList={chatList} selectedChat={selectedChat} selectChat={onSelectChat} />
+            <div
+              className="col-lg-4 col-xs-12 bg-light"
+              style={{ height: 658 }}
+            >
+              <div className="row p-3">
+                <h2>Recent Chat</h2>
+              </div>
+              <div
+                className="row ml-0 mr-0 h-75 bg-white border rounded"
+                style={{ height: "100%", overflow: "auto" }}
+              >
+                <ChatList
+                  chatList={chatList}
+                  selectedChat={selectedChat}
+                  selectChat={onSelectChat}
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/ChatBox.js
+++ src/ChatBox.js
@@ -1,22 +1,21 @@
 import React from "react";
-import { username } from './config';
 
 const ChatBox = ({ chat }) => {
-    console.log(chat);
-    return (
-        <div className='col-xl-12'>
-            {
-                chat
-                    .map(chat =>
-                        <div key={chat.id} className="message">
-                            <div className={`${chat.receiver !== 'admin' ? 'balon1' : 'balon2'} p-3 m-1`}>
-                                {chat.text}
-                            </div>
-                        </div>)
-            }
+  return (
+    <div className="col-xl-12">
+      {chat.map((chat, index) => (
+        <div key={index} className="message">
+          <div
+            className={`${
+              chat.type !== "receive" ? "balon1" : "balon2"
+            } p-3 m-1`}
+          >
+            {chat.text}
+          </div>
         </div>
-    )
-    // }
-}
+      ))}
+    </div>
+  );
+};
 
 export default ChatBox;
